e01ff306f747c379d8f958073a84317d4ec95528
Javier Torres <javierandrestorresreyes@gmail.com>
2018-11-15 10:53:32 -0500

    dfs

--- FP-Lenis-Palma-Torres/src/main/java/model/AdjListGraph.java
+++ FP-Lenis-Palma-Torres/src/main/java/model/AdjListGraph.java
@@ -224,7 +224,32 @@ public class AdjListGraph<T> implements IGraph<T> {
 	}
 
 	public void dfs() {
-
+		for (Vertex<T> u : vertices) {
+			u.setColor(Vertex.WHITE);
+			u.setPred(null);
+		}
+		int time = 0;
+		for (Vertex<T> u : vertices) {
+			if(u.getColor() == Vertex.WHITE)
+				time = dfsVisit((AdjVertex<T>)u, time);
+		}
+	}
+	
+	private int dfsVisit(AdjVertex<T> u, int time) {
+		time++;
+		u.setD(time);
+		u.setColor(Vertex.GRAY);
+		for (int i = 0; i < u.getAdjList().size(); i++) {
+			AdjVertex<T> v = (AdjVertex<T>) u.getAdjList().get(i).getDestination();
+			if(v.getColor() == Vertex.WHITE) {
+				v.setPred(u);
+				time = dfsVisit(v, time);
+			}
+		}
+		u.setColor(Vertex.BLACK);
+		time++;
+		u.setF(time);
+		return time;
 	}
 
 	public double[] dijkstra(AdjVertex<T> vertex) {
@@ -276,5 +301,7 @@ public class AdjListGraph<T> implements IGraph<T> {
 
 		return dist;
 	}
+	
+	
 
 }
