1cd8f39e29d4ec0cf57e00d9010e418ef3fdda00
camilaleniss <mlmariacami2@gmail.com>
2018-11-23 15:51:50 -0500

    Added metodo para encontrar camino min
    Se modifico el constructor de la mansión y demás

--- FP-Lenis-Palma-Torres/src/main/java/model/Mansion.java
+++ FP-Lenis-Palma-Torres/src/main/java/model/Mansion.java
@@ -1,23 +1,83 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Mansion {
-	public String ADJMATRIX="adjMatrix";
-	public String ADJLIST="adjList";
+	public static boolean ISLIST=true;
 	
-	private String name;
-	private IGraph<Room> graph;
+	private AdjListGraph<Room> adjList;
+	private AdjMatrixGraph<Room> adjMatrix;
 	
-	public Mansion(String n, String gt, boolean d, boolean w) {
-		name=n;
-		if(gt==ADJMATRIX) {
-			graph= new AdjMatrixGraph(d,w);
-		}else if(gt==ADJLIST) {
-			graph=new AdjListGraph(d,w);
+	private ArrayList<Vertex<Room>> exits;
+	
+	private boolean state;
+	
+	
+	public Mansion() {
+		adjList = new AdjListGraph<>(true, true);
+		adjMatrix = new AdjMatrixGraph<>(true, true);
+		exits = new ArrayList<>();
+		state=ISLIST;
+		initMansion();
+	}
+	
+	
+	/**
+	 * This method changes the state of the Graph used from AdjList to AdjMatrix and viceversa
+	 */
+	public void changeState() {
+		if (state) {
+			state=false;
+		}else {
+			state=ISLIST;
 		}
 	}
-//	public Room search() {
-//		
-//	}
+	
+	/**
+	 * This method find the minimum path to an exit from the mansion
+	 * The mansion can has many exits, so it finds the minimum time with all the different options
+	 * @param room The name of the room where it wants to exit
+	 * @return The minimum time to an exit
+	 */
+	public  double shortestWayOut(String room) {
+		Vertex<Room> rr= search(room);
+		int indexrr=0;
+		double time=0, mintime=IGraph.INF;
+		double[][] times;
+			//It does the FloydWarshall algorithm to find the shortest part from all the exits
+			if (state) {
+				times= adjList.floydwarshall();
+				indexrr=adjList.getIndexOf(rr);
+			}else {
+				times=adjMatrix.floydwarshall();
+				indexrr=adjMatrix.getIndexOf(rr);
+			}
+			//Here it search the shortest path from the all the exits
+			for (int i=0; i<exits.size(); i++) {
+				int index;
+				if(state)
+					index=adjList.getIndexOf(exits.get(i));
+				else
+					index=adjMatrix.getIndexOf(exits.get(i));	
+				time = times[indexrr][index];
+				mintime = Math.min(time, mintime);
+			}
+		return mintime;
+	}
+	
+	/**
+	 * This method finds the vertex of the room given the name of it.
+	 * @param room The name of the room
+	 * @return The vertex where is saved the room
+	 */
+	public Vertex<Room> search(String room) {
+		for (int i=0; i<adjList.getNumberOfVertices(); i++) {
+			if (adjList.getVertices().get(i).getValue().getName().equals(room))
+				return adjList.getVertices().get(i);
+		}
+		return null;
+	}
+	
 	
 	public void addRoom() {
 		
@@ -39,7 +99,9 @@ public class Mansion {
 	public void announceClosure() {
 		
 	}
-	public  void shortestWayOut() {
+	
+	public void initMansion() {
 		
 	}
+	
 }
--- FP-Lenis-Palma-Torres/src/main/java/model/Room.java
+++ FP-Lenis-Palma-Torres/src/main/java/model/Room.java
@@ -3,30 +3,43 @@ package model;
 import java.util.ArrayList;
 
 public class Room {
-	public String ADJMATRIX="adjmatrix";
-	public String ADJLIST="adjlist";
 	
-	private String type;
-	private ArrayList<Treasure> valuables;
+	private String name;
 	
-	public Room(String t) {
-		type=t;
-		valuables=new ArrayList<Treasure>();
+	private ArrayList<Treasure> treasures;
+	
+	private boolean isExit;
+	
+	public Room(String name, boolean isExit) {
+		this.name=name;
+		this.isExit=isExit;
+		treasures=new ArrayList<Treasure>();
 	}
-	public String getType() {
-		return type;
+	
+	public String getName() {
+		return name;
 	}
-	public ArrayList<Treasure> getValuables(){
-		return valuables;
+	
+	public ArrayList<Treasure> getTreasures(){
+		return treasures;
 	}
-	public void setType(String n) {
-		type=n;
+	
+	public void setName(String name) {
+		this.name=name;
 	}
+	
+	public boolean isExit() {
+		return isExit;
+	}
+	
 	public void addValuable(String n, double v) {
 		Treasure newTreasure= new Treasure(n,v);
 		addValuable(newTreasure);
 	}
-	public void addValuable(Treasure t) {
-		valuables.add(t);
+	
+	private void addValuable(Treasure treasure) {
+		treasures.add(treasure);
 	}
+	
+	
 }
