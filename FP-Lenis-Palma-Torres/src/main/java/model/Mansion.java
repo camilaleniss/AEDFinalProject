1ce0bd9d949a863e8f4233b5a379158891541282
camilaleniss <mlmariacami2@gmail.com>
2018-11-25 20:52:10 -0500

    Empezados diseños casos MansionTest

--- FP-Lenis-Palma-Torres/src/main/java/model/Mansion.java
+++ FP-Lenis-Palma-Torres/src/main/java/model/Mansion.java
@@ -4,47 +4,20 @@ import java.util.*;
 
 public class Mansion {
 
-//	private AdjListGraph<Room> adjList;
-//	private AdjMatrixGraph<Room> adjMatrix;
 	private IGraph<Room> graph;
 
-//	private ArrayList<Vertex<Room>> exits;
 	private HashMap<String, Room> mapRooms;
 	private List<Treasure> museum;
 
 	private boolean isList;
 
-//	public Mansion() {
-//		adjList = new AdjListGraph<>(true, true);
-//		adjMatrix = new AdjMatrixGraph<>(true, true);
-//		mapRooms = new HashMap<>();
-//		exits = new ArrayList<>();
-//		state=ISLIST;
-//		initMansion();
-//	}
-
 	public Mansion(boolean isList) {
-//		adjList = new AdjListGraph<>(true, true);
-//		adjMatrix = new AdjMatrixGraph<>(true, true);
 		graph = isList ? new AdjListGraph<>(true, true) : new AdjMatrixGraph<>(true, true);
 		mapRooms = new HashMap<>();
 		museum = new ArrayList<>();
-//		exits = new ArrayList<>();
 		this.isList = isList;
 		initMansion();
 	}
-//
-//	/**
-//	 * This method changes the state of the Graph used from AdjList to AdjMatrix and
-//	 * viceversa
-//	 */
-//	public void changeState() {
-//		if (state) {
-//			state = false;
-//		} else {
-//			state = ISLIST;
-//		}
-//	}
 
 	/**
 	 * This method find the minimum path to an exit from the mansion The mansion can
@@ -82,28 +55,6 @@ public class Mansion {
 		}
 
 		return path;
-//		int indexrr=0;
-//		double time=0, mintime=IGraph.INF;
-//		double[][] times;
-//			//It does the FloydWarshall algorithm to find the shortest part from all the exits
-//			if (state) {
-//				times= adjList.floydwarshall();
-//				indexrr=adjList.getIndexOf(rr);
-//			}else {
-//				times=adjMatrix.floydwarshall();
-//				indexrr=adjMatrix.getIndexOf(rr);
-//			}
-//			//Here it search the shortest path from the all the exits
-//			for (int i=0; i<exits.size(); i++) {
-//				int index;
-//				if(state)
-//					index=adjList.getIndexOf(exits.get(i));
-//				else
-//					index=adjMatrix.getIndexOf(exits.get(i));	
-//				time = times[indexrr][index];
-//				mintime = Math.min(time, mintime);
-//			}
-//		return mintime;
 	}
 
 	public double getPathLength(List<Room> path) throws NotFoundException {
@@ -125,22 +76,11 @@ public class Mansion {
 	 * @throws NotFoundException If the room does not exist
 	 */
 	public Vertex<Room> search(String room) {
-
-//		for (int i=0; i<adjList.getNumberOfVertices(); i++) {
-//			if (adjList.getVertices().get(i).getValue().getName().equals(room))
-//				return adjList.getVertices().get(i);
-//		}
-//		return null;
 		Room r = searchRoom(room);
 		return r == null ? null : graph.searchVertex(r);
 	}
 
 	public Room searchRoom(String room) {
-//		for (int i=0; i<adjList.getNumberOfVertices(); i++) {
-//			if (adjList.getVertices().get(i).getValue().getName().equals(room))
-//				return adjList.getVertices().get(i).getValue();
-//		}
-//		return null;
 		return mapRooms.get(room);
 	}
 
@@ -151,10 +91,6 @@ public class Mansion {
 		Room r = new Room(name, isExit);
 		mapRooms.put(name, r);
 		graph.addVertex(r);
-//		if (isExit)
-//			exits.add(graph.searchVertex(r));
-//		adjList.addVertex(new Room(name, isExit));
-//		adjMatrix.addVertex(new Room(name, isExit));
 	}
 
 	public void deleteCorridor(String from, String to) throws NotFoundException {
@@ -181,8 +117,6 @@ public class Mansion {
 			throw new CorridorAlreadyExistsException();
 		}
 		graph.addEdge(fromRoom, toRoom, time);
-//		adjList.addEdge(fromvertex, tovertex, time);
-//		adjMatrix.addEdge(fromvertex, tovertex, time);
 
 	}
 
@@ -294,19 +228,6 @@ public class Mansion {
 		}
 		return neigh;
 	}
-
-//	public List<Room> getFromNeighbors(Room r){
-//		ArrayList<Room> neigh = new ArrayList<>();
-//		Vertex<Room> v = graph.searchVertex(r);
-//		if(v != null) {
-//			List<Vertex<Room>> vertices = graph.getVertices();
-//			for(Vertex<Room> u : vertices) {
-//				if(graph.areAdjacent(u, v))
-//					neigh.add(u.getValue());
-//			}
-//		}
-//		return neigh;
-//	}
 	
 	public void addTreasure(String room, String name, double value) throws NotFoundException {
 		Room r = searchRoom(room);
